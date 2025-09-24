c86dc70c3099f5bef580167d29b224d659411ee1
AmanDevelops <amanpal2072004@gmail.com>
2025-09-24 23:25:17 +0530

    final hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,16 @@
 "use client";
 
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import Link from "next/link";
 import { WalletSelector } from "./WalletSelector";
 
 export function Header() {
+
   return (
     <nav className="p-4 md:p-6 font-display dark:bg-gray-900 shadow ">
       <div className="container mx-auto flex flex-wrap md:flex-nowrap justify-between items-center ">
         {/* Title */}
-        <Link
-          href="/"
-          className="text-2xl font-bold gradient-text"
-        >
+        <Link href="/" className="text-2xl font-bold gradient-text">
           SlashContract
         </Link>
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import Link from "next/link";
 
 export default function Hero() {
+  const { account, connected, disconnect, wallet } = useWallet();
+
   return (
     <main className="flex-grow flex flex-col items-center justify-center h-[60vh] max-sm:h-[42vh]">
       <section className="text-center flex flex-col items-center justify-end">
@@ -9,16 +12,16 @@ export default function Hero() {
           Decentralizing Trust Between <br /> Employers and Workers.
         </h1>
         <p className="mt-5 text-xl font-semibold text-gray-200 text-center tracking-tight leading-6 max-w-2xl">
-          Anonmily — Your thoughts, your rules, your mystery.
+          SlashContract - Next-Generation Escrow, Powered by Aptos Move.
         </p>
 
         <div className="mt-8 flex gap-4 flex-col sm:flex-row">
           <Button
             asChild
             size="lg"
-            className="bg-gradient-to-b from-black to-[#001E80] dark:from-[#6EA8FF] dark:to-[#0044CC] text-white dark:text-black hover:from-gray-900 hover:to-[#002a99] dark:hover:from-[#8EBFFF] dark:hover:to-[#0055FF] transition-all"
+            className="gradient-button"
           >
-            <Link href="/auth/register">Get Started</Link>
+            <Link href="/auth/register">{connected ? "Dashboard" : "Get Started"}</Link>
           </Button>
 
           {/* <Button asChild variant="outline" size="lg" className="dark:border-gray-500 dark:text-gray-200">
